e1ef683af212bdbf7ce690161d65d09cff498e0a
Rakshitshetty123 <rakshit.shetty@lntinfotech.com>
2021-03-23 11:05:58 +0530

    second

--- Azuredemo/Program.cs
+++ Azuredemo/Program.cs
@@ -11,6 +11,7 @@ namespace Azuredemo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World11");
         }
     }
 }
